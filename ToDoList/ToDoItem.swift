eec30f938c117abbc48402dad2619f7e256b7f27
davidgertmenianwong <dgertmenianwong@gmail.com>
2015-02-05 13:53:12 -0800

    Add ToDoItem

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Dave Gertmenian-Wong on 2/5/15.
+//  Copyright (c) 2015 Dave Gertmenian-Wong. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+   
+}
