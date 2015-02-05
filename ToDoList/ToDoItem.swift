d7de386a98b7b330079b745d7e6e1753621b8d8a
davidgertmenianwong <dgertmenianwong@gmail.com>
2015-02-05 13:54:12 -0800

    Configure ToDoItem class

--- ToDoList/ToDoItem.swift
+++ ToDoList/ToDoItem.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class ToDoItem: NSObject {
-   
+    let itemName: String
+    var completed: Bool
+    
+    init(itemName: String, completed: Bool=false) {
+        self.itemName = itemName
+        self.completed = completed
+    }
 }
